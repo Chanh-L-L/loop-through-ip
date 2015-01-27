c919b9edae5a107c918d295cae19982545e97b49
Chanh Long Lu <wizard-9@hotmail.com>
2015-01-27 01:25:29 +0000

    pipe to stderr, show dns, remove output.txt and tidy up

--- LoopIP.sh
+++ LoopIP.sh
@@ -1,15 +1,25 @@
-#!bin/sh
+#!/bin/bash
 
 ipaddress="130.216.158"
+ports=(80 443)
 
 func_curl() {
-	curl -m 1 "$1:$2"
+	ipaddr=$1
+	port=$2
+	
+	# Time in seconds
+	duration=1
+	
+	# use sed to take the first dns and then remove a dot at the end of that dns
+	dns=$(dig -x $ipaddr +short | sed -n 1p | sed '$s/\.$//')
+
+	curl -m $duration "$ipaddr:$port" &> /dev/null	
 	
 	if [ "$?" = "0" ]
 	then
-		echo "$1:$2 OK" >> output.txt
+		echo "$ipaddr,$dns,$port,OK"
 	else 
-		echo "$1:$2 FAIL" >> output.txt
+		echo "$ipaddr,$dns,$port,FAIL"
 	fi
 }
 
@@ -19,8 +29,10 @@ do
 	
 	if [ "$ipaddress.$suffix" != "$ipaddress.0" ] && [ "$ipaddress.$suffix" != "$ipaddress.255" ]
 	then
-		func_curl "$ipaddress.$suffix" 80
-		func_curl "$ipaddress.$suffix" 443
+		for p in ${ports[@]}
+		do
+			func_curl "$ipaddress.$suffix" "$p"
+		done
 	fi
 	
 	# Move to 130.216.159
